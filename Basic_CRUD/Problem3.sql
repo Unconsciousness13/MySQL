392099417f23b6c1d2e2b67af2b012a6ede3cebf
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-01-09 13:06:38 +0100

    Lab

--- Basic_CRUD/Problem3.sql
+++ Basic_CRUD/Problem3.sql
@@ -1,11 +1,9 @@
-ALTER TABLE `employees` 
+UPDATE `employees` 
 SET 
-    `salary` = `salary` + 100
+    salary = salary + 100
 WHERE
-    `job_title` = 'Manager';
- 
+    job_title = 'Manager';
 SELECT 
     `salary`
 FROM
-    `employees`
-ORDER BY `salary` ASC;
+    `employees`;
