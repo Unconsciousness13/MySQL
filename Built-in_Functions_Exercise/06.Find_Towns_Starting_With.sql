c4abaea3bf8da913093a3e2a9fe20dc6d3e46a04
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-01-21 12:36:05 +0100

    Exercise

--- Built-in_Functions_Exercise/06.Find_Towns_Starting_With.sql
+++ Built-in_Functions_Exercise/06.Find_Towns_Starting_With.sql
@@ -20,3 +20,10 @@ WHERE
     OR LOWER(name) LIKE  'b%' 
     OR LOWER(name) LIKE 'e%' 
 ORDER BY name ASC;
+
+--- third solution
+
+SELECT town_id,name
+FROM towns
+WHERE LEFT(name,1) IN ('M','K','B','E')
+ORDER BY name
