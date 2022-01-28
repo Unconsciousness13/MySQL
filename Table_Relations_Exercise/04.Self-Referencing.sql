d6cf5d4351d2ae93614a5a35e024ffbfddc2f5b6
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-01-28 14:08:18 +0100

    Exercise

---
+++ Table_Relations_Exercise/04.Self-Referencing.sql
@@ -0,0 +1,19 @@
+CREATE TABLE teachers(
+	`teacher_id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    `manager_id` INT
+) AUTO_INCREMENT = 101;
+
+INSERT INTO teachers(`name`, manager_id)
+VALUES
+('John', NULL),
+('Maya', 106),
+('Silvia', 106),
+('Ted', 105),
+('Mark', 101),
+('Greta', 101);
+
+ALTER TABLE teachers
+ADD CONSTRAINT fk_manager_id_teacher_id
+FOREIGN KEY teachers(manager_id)
+REFERENCES teachers(teacher_id);
