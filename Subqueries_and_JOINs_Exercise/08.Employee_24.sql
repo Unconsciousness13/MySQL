676981ec7815e1f0b7323651be891dacc2d12535
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-02-01 13:49:30 +0100

    Exercise

--- Subqueries_and_JOINs_Exercise/08.Employee_24.sql
+++ Subqueries_and_JOINs_Exercise/08.Employee_24.sql
@@ -14,3 +14,20 @@ WHERE
     e.employee_id = 24
 ORDER BY
     p.name;
+
+----------------------------------------------------------------
+SELECT
+    e.employee_id,
+    e.first_name,
+    CASE
+        WHEN YEAR(p.start_date) >= 2005 THEN null
+        ELSE p.name
+    END AS project_name
+FROM
+    employees AS e
+    JOIN employees_projects AS ep ON e.employee_id = ep.employee_id
+    JOIN projects AS p ON p.project_id = ep.project_id
+WHERE
+    e.employee_id = 24
+ORDER BY
+    p.name
