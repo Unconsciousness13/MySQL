cbfb8a9ff526b4e05b38e4ef289d5c046e779b31
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-02-01 12:54:51 +0100

    Exercise

---
+++ Subqueries_and_JOINs_Exercise/15.*Continents_and_Currencies.sql
@@ -0,0 +1,28 @@
+SELECT
+    c.continent_code,
+    c.currency_code,
+    COUNT(*) AS 'currency_usage'
+FROM
+    `countries` AS c
+GROUP BY
+    c.continent_code,
+    c.currency_code
+HAVING
+    `currency_usage` > 1
+    AND `currency_usage` = (
+        SELECT
+            COUNT(*) AS cn
+        FROM
+            `countries` AS c2
+        WHERE
+            c2.continent_code = c.continent_code
+        GROUP BY
+            c2.currency_code
+        ORDER BY
+            cn DESC
+        LIMIT
+            1
+    )
+ORDER BY
+    c.continent_code,
+    c.continent_code;
