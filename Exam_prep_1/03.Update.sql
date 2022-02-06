9e6ba43d1ed73db02f48d9a134b0c2112ff3135d
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-02-06 21:39:03 +0100

    Exam prep

--- Exam_prep_1/03.Update.sql
+++ Exam_prep_1/03.Update.sql
@@ -1,8 +1,8 @@
 UPDATE
-    coaches as c
-    JOIN players_coaches aAS pc ON pc.coach_id = c.id
+    coaches AS c
+    JOIN players_coaches AS pc ON pc.coach_id = c.id
 SET
-    c.coach_level = coach_level + 1
+    c.coach_level = c.coach_level + 1
 WHERE
     c.coach_level >= 1
     AND c.first_name LIKE 'A%';
