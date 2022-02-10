913fee920fe9a35f6de081aac07d886c674b6626
Plamen Iliev <77366596+Unconsciousness13@users.noreply.github.com>
2022-02-10 15:11:12 +0100

    Exam prep

---
+++ Database_Basics_MySQL_Retake_Exam_06_August_2021/11.Update_Budget_of_the_Games.sql
@@ -0,0 +1,19 @@
+DELIMITER $ $ CREATE PROCEDURE udp_update_budget(min_game_rating FLOAT) BEGIN
+UPDATE
+    games
+SET
+    budget = budget + 100000.00,
+    release_date = DATE_ADD(release_date, INTERVAL 1 YEAR)
+WHERE
+    rating > min_game_rating
+    AND release_date IS NOT NULL
+    AND id NOT IN (
+        SELECT
+            game_id
+        FROM
+            games_categories
+    );
+
+END $ $ DELIMITER;
+
+CALL udp_update_budget (8);
